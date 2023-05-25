6be8b1fbd8aaa5e7219bd463c87392964db468c2
Walter Barbieri <128515233+uolli90@users.noreply.github.com>
2023-05-25 17:20:20 +0200

    WalterWeek9Day4Esercizio Commit no display

---
+++ day4/esercizio/assets/ts/index.ts
@@ -0,0 +1,53 @@
+window.onload = async function () {
+  await fetch("./Abbigliamento.json", {
+    method: "GET",
+  })
+    .then((raw) => {
+      return raw.json();
+    })
+    .then((response: any) => {
+      display(response);
+    });
+};
+
+class SaldoCapo {
+  constructor(
+    public capo: string,
+    public codprod: number,
+    public collezione: string,
+    public colore: string,
+    public disponibile: string,
+    public id: number,
+    public modello: number,
+    public prezzoivaesclusa: number,
+    public prezzoivainclusa: number,
+    public quantita: number,
+    public saldo: number
+  ) {}
+  getsaldocapo() : number {
+    return (this.prezzoivainclusa * this.saldo)/100;
+  }
+  getacquistocapo(): number {
+    return this.prezzoivainclusa - this.getsaldocapo();
+  }
+}
+
+function display(elements: any) {
+    return elements.forEach((element: any) => {
+        element = new SaldoCapo(element.capo,
+            element.codprod,
+            element.collezione,
+            element.colore,
+            element.disponibile,
+            element.id,
+            element.modello,
+            element.prezzoivaesclusa,
+            element.prezzoivainclusa,
+            element.quantita,
+            element.saldo)
+            console.log(element);            
+            console.log('Il saldo dell\'elemento ' + element.id + ' è ' + element.getsaldocapo());
+            console.log('Il prezzo scontato dell\'elemento ' + element.id + ' è ' + element.getacquistocapo());
+    });
+    
+}
